100e91ed761af3b95faa8d6d58e3f9c973e61051
Luis Enrique Rendon Cortez <luis.rendon@justo.mx>
2023-07-31 05:14:00 -0600

    delete comments

--- app/src/androidTest/kotlin/LocalDataSourceTest.kt
+++ app/src/androidTest/kotlin/LocalDataSourceTest.kt
@@ -17,11 +17,6 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class LocalDataSourceTest {
 
